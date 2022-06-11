3fdae022f4aa0dbd9e8270bff1f95122cad5d58b
Claire Pan <clairewww28@gmail.com>
2022-06-11 19:03:08 +0800

    Change text

--- ColorChange/ViewController.swift
+++ ColorChange/ViewController.swift
@@ -55,7 +55,7 @@ class ViewController: UIViewController {
         alphaLabel.text = String(format: "%.2f", alphaSlider.value)
     }
     
-    @IBAction func changeGragientColor(_ sender: Any) {
+    @IBAction func gradientColorControl(_ sender: Any) {
         if gradientSwitch.isOn{
             redSlider.isEnabled = false
             greenSlider.isEnabled = false
